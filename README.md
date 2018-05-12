001f35538a833c16d7da8519e97df4aea9f8addd
Tyler Folkman <tylerfolkman@gmail.com>
2018-05-11 21:42:18 -0600

    readme

---
+++ README.md
@@ -0,0 +1,6 @@
+Next Ideas:
+1. https://pytorch.org/tutorials/beginner/nlp/advanced_tutorial.html
+2. https://pytorch.org/tutorials/intermediate/spatial_transformer_tutorial.html
+3. https://pytorch.org/tutorials/advanced/neural_style_tutorial.html
+4. Fast ai course 2
+5. Andrew Ng deep learning course
--- models.py
+++ models.py
@@ -19,6 +19,41 @@ class DQN(nn.Module):
         x = F.relu(self.bn3(self.conv3(x)))
         return self.head(x.view(x.size(0), -1))
 
+    
+class ConvNet(nn.Module):
+
+    def __init__(self, n_actions):
+        super(ConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=5, stride=2)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
+        self.bn3 = nn.BatchNorm2d(32)
+        self.head = nn.Linear(1568, n_actions)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        return F.sigmoid(self.head(x.view(x.size(0), -1)))
+
+    
+class CartPolicy(nn.Module):
+    def __init__(self, n_inputs=4, n_hidden=4, n_outputs=2):
+        super(CartPolicy, self).__init__()
+        self.n_inputs = n_inputs
+        self.n_hidden = n_hidden
+        self.n_outputs = n_outputs
+        
+        self.hidden1 = nn.Linear(n_inputs, n_hidden)
+        self.out = nn.Linear(n_hidden, n_outputs)
+        
+    def forward(self, x):
+        output = F.elu(self.hidden1(x))
+        output = self.out(output)
+        return F.softmax(output, dim=1)
+
  
 class NNPolicy(nn.Module):
     def __init__(self, n_inputs=4, n_hidden=4, n_outputs=2):
